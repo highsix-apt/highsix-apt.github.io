4afc2439026706fa543778382e7ecf8268f212a1
IV <153679633+highsix-apt@users.noreply.github.com>
2024-01-25 14:25:47 -0300

    fix: ajuste overlay

--- js/overlay.js
+++ js/overlay.js
@@ -1,22 +1,19 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const overlay = document.querySelector(".overlay");
-//   const removeOverlayButton = document.querySelector(".delete-overlay");
+const access = localStorage.getItem("access") || "false";
+localStorage.setItem("access", access);
 
-//   const access = localStorage.getItem("access") || "false";
+document.body.style.opacity = access === "true" ? 0 : 1;
 
-//   localStorage.setItem("access", access);
+const overlay = document.querySelector(".overlay");
+const removeOverlayButton = document.querySelector(".delete-overlay");
 
-//   overlay.style.display = access === "true" ? "none" : "flex";
+overlay.style.display = access === "true" ? "none" : "flex";
 
-//   document.body.style.opacity = access === "true" ? 0 : 1;
+removeOverlayButton.addEventListener("click", function () {
+  localStorage.setItem("access", "true");
+  location.reload();
+});
 
-//   removeOverlayButton.addEventListener("click", function () {
-//     localStorage.setItem("access", "true");
-//     location.reload();
-//   });
-
-//   setTimeout(function () {
-//     document.body.style.opacity = 1;
-//     localStorage.setItem("access", "false");
-//   }, 2000);
-// });
+setTimeout(function () {
+  document.body.style.opacity = 1;
+  localStorage.setItem("access", "false");
+}, 2000);
