42aea3145292b4b4cb9e72d771a56919bfc33154
highsix-apt <153679633+highsix-apt@users.noreply.github.com>
2026-01-31 18:03:08 -0300

    fix: ajuste audio

--- js/overlay.js
+++ js/overlay.js
@@ -1,17 +1,23 @@
 const overlay = document.querySelector(".overlay");
 const button = document.querySelector(".delete-overlay");
+const page = document.querySelector(".page-content");
+const audio = document.getElementById("player");
 
 const hasAccess = sessionStorage.getItem("access") === "true";
 
 if (hasAccess) {
   overlay.style.display = "none";
-  document.querySelector(".page-content").classList.add("fade-in");
+  page.classList.add("fade-in");
+  audio.play();
 } else {
   overlay.style.display = "flex";
+  audio.pause();
 }
 
 button.addEventListener("click", () => {
   sessionStorage.setItem("access", "true");
+
+  audio.play();
 
   overlay.classList.add("fade-out");
 
@@ -19,7 +25,7 @@ button.addEventListener("click", () => {
     "transitionend",
     () => {
       overlay.style.display = "none";
-      document.querySelector(".page-content").classList.add("fade-in");
+      page.classList.add("fade-in");
     },
     { once: true },
   );
