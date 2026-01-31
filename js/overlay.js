094674f21b249dc16226998e5ebc2827a22e8c76
highsix-apt <153679633+highsix-apt@users.noreply.github.com>
2026-01-31 19:13:36 -0300

    chore: adiciona musica e som de chuva

--- js/overlay.js
+++ js/overlay.js
@@ -1,23 +1,29 @@
 const overlay = document.querySelector(".overlay");
 const button = document.querySelector(".delete-overlay");
 const page = document.querySelector(".page-content");
-const audio = document.getElementById("player");
+const music = document.getElementById("music");
+const rain = document.getElementById("rain");
 
 const hasAccess = sessionStorage.getItem("access") === "true";
+
+rain.volume = 0.5;
 
 if (hasAccess) {
   overlay.style.display = "none";
   page.classList.add("fade-in");
-  audio.play();
+  music.play();
+  rain.play();
 } else {
   overlay.style.display = "flex";
-  audio.pause();
+  music.pause();
+  rain.pause();
 }
 
 button.addEventListener("click", () => {
   sessionStorage.setItem("access", "true");
 
-  audio.play();
+  music.play();
+  rain.play();
 
   overlay.classList.add("fade-out");
 
